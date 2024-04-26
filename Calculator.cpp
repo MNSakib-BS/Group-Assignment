65164296c6de5f850cbcaa1e77da9f406ba55ead
Akib <ali.haider@brainstation-23.com>
2024-04-26 11:00:54 +0600

    Division function done by BS1542

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int division(int dividend, int divisor) {
+    if (divisor != 0) {
+        return dividend / divisor; 
+    } else {
+        std::cerr << "Error: Division by zero!" << std::endl;
+        return 0; 
+    }
+}
+
+
 int main() {
     cout << "Enter your math expression, e.g., 5 + 2" << endl;
     int firstNumber, secondNumber;
